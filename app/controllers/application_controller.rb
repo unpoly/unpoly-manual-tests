e3be1953fe38ad4bcf95fc3a3f7eef87bb1d67b7
Henning Koch <henning.koch@makandra.de>
2021-08-27 11:29:37 +0200

    Upgrade to Rails 5.2, Ruby 2.5

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_filter :log_content_type
+  before_action :log_content_type
 
   private
 
